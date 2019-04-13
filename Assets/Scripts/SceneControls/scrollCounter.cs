5630866493cf9377667eb03913a93b18d5b7909d
fdefendi <fdefendi@atra.it>
2019-04-14 01:27:34 +0200

    added prereqs to open the door, sora

--- Assets/Scripts/SceneControls/scrollCounter.cs
+++ Assets/Scripts/SceneControls/scrollCounter.cs
@@ -6,12 +6,22 @@ using UnityEngine;
 public class scrollCounter : MonoBehaviour
 {
     [SerializeField] private GameObject[] scrollsToCollect;
+    [SerializeField] private GameObject objectToEnable;
 
     public void collectScroll(GameObject collectedObject)
     {
-        scrollsToCollect = Array.FindAll(scrollsToCollect, (GameObject element) => 
+        scrollsToCollect = Array.FindAll(scrollsToCollect, (GameObject element) =>
         {
             return element.GetInstanceID() != collectedObject.GetInstanceID();
         });
+        if (scrollsToCollect.Length == 0)
+        {
+            NoScrolls();
+        }
+    }
+
+    private void NoScrolls()
+    {
+        objectToEnable.SetActive(true);
     }
 }
