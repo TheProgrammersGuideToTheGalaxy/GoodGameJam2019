26bf2a2bbd5c1076c239142c0aff559d2d5fa869
Sergio R. Lumley <lumley256@gmail.com>
2019-04-13 11:55:15 +0200

    Importing platform elements, created small character controller snippet

---
+++ Assets/Scripts/CharacterControls/CharacterMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            // Start jump here
+        }
+    }
+}
