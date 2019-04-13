52b1492f555c144d58dcc70d21d695be2aa5ca5f
fdefendi <fdefendi@atra.it>
2019-04-13 17:48:08 +0200

    fixed small bug in collision_detector

--- Assets/Collision_Detector.cs
+++ Assets/Collision_Detector.cs
@@ -14,6 +14,6 @@ public class Collision_Detector : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        OnCollisionExit2D(collision);
+        upperClass.OnCollisionExit2D(collision);
     }
 }
